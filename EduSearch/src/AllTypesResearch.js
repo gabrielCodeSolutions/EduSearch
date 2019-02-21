bb812fbe92745b607c7fe757fae8b6d22b9d9054
Gabriel Menezes <gabrielmenezes@MacBook-Pro-de-Gabriel.local>
2019-02-20 21:20:48 -0300

    Picker de filtro de pesquisa; Lista de artigos com scroll, integracao de teste

--- EduSearch/src/AllTypesResearch.js
+++ EduSearch/src/AllTypesResearch.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, TextInput, TouchableOpacity, ScrollView} from 'react-native';
+import {View, StyleSheet, TextInput, TouchableOpacity, ScrollView, Text} from 'react-native';
 import Icon from "react-native-vector-icons/FontAwesome";
 import axios from 'axios';
 import strings from './util/Strings';
+import RNPickerSelect from 'react-native-picker-select';
+
 
 export default class AllTypesResearch extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            articles: []
+            articles: [],
+            searchType: ""
         }
     }
 
@@ -25,7 +28,9 @@ export default class AllTypesResearch extends Component {
             })
     }
 
+openArticle = () => {
 
+};
     render() {
         return (
             <View style={styles.mainContainer}>
@@ -45,12 +50,72 @@ export default class AllTypesResearch extends Component {
                         />
                     </View>
                 </View>
-                <ScrollView>
+                <View style={styles.researchSelectContainer}>
+                    <RNPickerSelect
+                        useNativeAndroidPickerStyle={false}
+                        style={{...pickerSelectStyles}}
+                        placeholder={{}}
+                        hideIcon={true}
+                        items={[{
+                            label: "Autor",
+                            value: 'Autor',
+                        },
+                            {
+                                label: "Palavra-chave",
+                                value: 'Palavra-chave',
+                            },
+                            {
+                                label: "Revista",
+                                value: "Revista"
+                            }
 
+                        ]}
+                        onValueChange={(value) => {
+                            this.setState({searchType: value});
+                        }}
+                        onUpArrow={() => {
+                            //  this.inputRefs.name.focus();
+                        }}
+                        onDownArrow={() => {
+                            //  this.inputRefs.picker2.togglePicker();
+                        }}
+
+                        value={this.state.searchType}
+
+                    />
+                    <View style={styles.dropDownIcon}>
+                        <Icon
+                            name={'sort-desc'} size={25} color={'#ffb131'}/>
+                    </View>
+                </View>
+                <ScrollView style={styles.articleListScrollContainer}>
+                    {
+                        this.state.articles.map((content, index) => {
+                            return (
+                                <View key={index} style={styles.articleListCell}>
+                                    <View style={{flex: 1}}>
+                                        <TouchableOpacity style={styles.seeArticleTitleButton}>
+                                            <Text style={styles.articleTitle}>
+                                                {content.title}
+                                            </Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                    <View style={{flex: 1, alignItems: 'flex-end'}}>
+                                        <TouchableOpacity style={styles.seeArticleDetails}>
+                                            <Text style={styles.articleDetailsText}>
+                                                Ver detalhes
+                                            </Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                            )
+
+
+                        })
+                    }
 
 
                 </ScrollView>
-
             </View>
 
         );
@@ -60,8 +125,18 @@ export default class AllTypesResearch extends Component {
 
 const styles = StyleSheet.create({
     mainContainer: {
-        backgroundColor: '#fdfdfd',
+        backgroundColor: '#fafafa',
         flex: 1
+    },
+    articleListScrollContainer: {
+        backgroundColor: "#fafafa",
+        marginTop: 20},
+    articleListCell: {
+        flex: 1,
+        height: 100,
+        flexDirection: 'row',
+        borderTopWidth: 1,
+        borderBottomWidth: 1
     },
     researchInputContainer: {
         backgroundColor: '#3866b5',
@@ -77,10 +152,75 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
 
     },
+    researchSelectContainer: {
+        backgroundColor: '#3866b5',
+        borderBottomWidth: 2,
+        borderBottomColor: '#ffb131',
+        height: 60,
+        marginRight: 50,
+        marginLeft: 50,
+        marginTop: 10,
+        borderRadius: 30,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    dropDownIcon: {
+        position: 'absolute',
+        right: 0,
+        marginRight: 20,
+        paddingBottom: 8
+    },
     researchInput: {
         paddingLeft: 10,
         paddingRight: 14,
         fontFamily: 'NotoSansSC-Black',
         color: "#fff"
+    },
+    articleTitle: {
+        fontSize: 10,
+        fontFamily: 'NotoSansSC-Bold'
+    },
+    seeArticleDetails: {
+        marginRight: 20,
+        marginTop: 32,
+
+    },
+    articleDetailsText: {
+        color: "#3866b5",
+        fontFamily: 'NotoSansSC-Light'
+
+    },
+    seeArticleTitleButton: {
+        marginLeft: 10,
+        marginTop: 30
     }
+
+});
+const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+        fontSize: 15,
+        paddingTop: 18,
+        paddingHorizontal: 50,
+        paddingBottom: 12,
+        borderWidth: 1,
+        borderColor: 'transparent',
+        borderRadius: 4,
+        backgroundColor: 'transparent',
+        color: 'white',
+        fontFamily: 'NotoSansSC-Black',
+
+    },
+    inputAndroid: {
+        fontSize: 16,
+        paddingTop: 13,
+        paddingHorizontal: 10,
+        paddingBottom: 12,
+        borderWidth: 1,
+        borderColor: 'transparent',
+        borderRadius: 4,
+        backgroundColor: 'transparent',
+        color: 'white',
+    },
 });
