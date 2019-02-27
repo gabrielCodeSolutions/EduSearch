0fc12a54ba955cc3f6b7d9c01bca00e2c779dfb1
Gabriel Menezes <gabrielmenezes@MacBook-Pro-de-Gabriel.local>
2019-02-27 20:27:28 -0300

    Firebase instalado no projeto

--- EduSearch/android/app/src/main/java/com/edusearch/MainApplication.java
+++ EduSearch/android/app/src/main/java/com/edusearch/MainApplication.java
@@ -3,6 +3,7 @@ package com.edusearch;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import org.wonday.pdf.RCTPdfView;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new RCTPdfView(),
             new RNFetchBlobPackage(),
             new VectorIconsPackage()
